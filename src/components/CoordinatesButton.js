93d709c78b8801cb2d4e9d16127f77ddea283c6d
rockshellS <spicer.rachelle@gmail.com>
2021-02-25 20:42:26 -0600

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,26 @@
-// Code CoordinatesButton Component Here
+import { render } from 'enzyme'
+import React, { Component } from 'react'
+
+
+class CoordinatesButton extends Component {
+    
+    handleClick = event => {
+    
+        this.props.onReceiveCoordinates([event.clientX, event.clientY]);
+    }
+
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleClick}>Cords</button>
+            </div>
+        )   
+    }
+
+}
+
+
+
+
+export default CoordinatesButton
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,26 @@
-// Code DelayedButton Component Here
+import { render } from 'enzyme'
+import React, { Component } from 'react'
+
+
+class DelayedButton extends Component {
+
+
+    handleClick = event => {
+        event.persist();
+        window.setTimeout(() => {
+          this.props.onDelayedClick(event);
+        }, this.props.delay);
+      };
+
+render() {
+    return(
+        <div>
+            <button onClick={this.handleClick}>Button</button>
+        </div>
+    )
+}
+
+}
+
+
+export default DelayedButton
